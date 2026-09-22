5ebc53315a09eaa3bc3b9fd20754c3081690ecc8
Ilham224 <muhammadilham080406@gmail.com>
2026-09-22 16:22:51 +0700

    Penambahan Nomor besar

---
+++ 1.py
@@ -0,0 +1,21 @@
+persentase= float(input("Enter Excellent performance: "))
+if persentase >= 90:
+    print("Excellent performance")
+
+persentase= float(input("Enter Very Good performance: "))
+if persentase >= 80:
+    print("Very Good performance")
+
+persentase= float(input("Enter Good performance: "))
+if persentase >= 70:
+    print("Good performance")
+
+persentase= float(input("Enter Average performance: "))
+if persentase >= 60:
+    print("Average performance")
+
+persentase= float(input("Enter Poor performance: "))
+if persentase >=50:
+    print("Poor performance")
+
+
---
+++ 4.py
@@ -0,0 +1,4 @@
+n = int(input("masukkan n: "))
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        print(i, end=" ")
